0b016d123a89244885d026e365bb4c3765c7b94b
Hitesh <hiteshkumartokasiya.edureka.co>
2025-09-09 15:54:47 +0530

    v1

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { use, useContext, useState } from "react";
 import "./Sidebar.css";
 import { assets } from "../../assets/assets"
-import { Context } from "../../context/context";
+import { Context } from "../../Context/context";
 const Sidebar = () => {
   const [extended, setExtended] = useState(false);
   const {onSent,prevPrompt,setRecentPrompt,newChat} = useContext(Context)
